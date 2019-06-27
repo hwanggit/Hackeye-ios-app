a944c58332f8fa4e151f7dece4e624953293c9bb
howard wang <luhao.wang2016@gmail.com>
2019-06-26 17:13:00 -0700

    Created 3-page UI layout

---
+++ Hackeye.io/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hackeye.io
+//
+//  Created by Howard Wang on 2019-06-26.
+//  Copyright © 2019 Howard Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
